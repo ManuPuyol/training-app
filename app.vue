17aeb0aba3088ed8b10f1391dac9d52216168207
manupuyol <manuelpuyol.dev@gmail.com>
2024-07-27 10:26:30 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-500">
+      ¡Hola, Nuxt 3 con Tailwind CSS!
+    </h1>
+  </div>
+</template>
